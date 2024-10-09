7593a8478c97cb36f7fd9ca7db637d18c053cc39
ashut <ashutosh.mall2@gmail.com>
2024-10-09 22:04:19 +0530

    as

---
+++ src/Oct/ex_03102024/Lab002.java
@@ -0,0 +1,13 @@
+package Oct.ex_03102024;
+/*This is multi line comments
+*This is use to Documentation,Reference
+* Author -Ashutosh
+* Ctrl+/
+ */
+public class Lab002 {
+    //Comments in java
+    //
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/Task/task_03102024/Task3.java
@@ -0,0 +1,11 @@
+package Task.task_03102024;
+
+public class Task3 {
+    public static void main(String[] args) {
+        // Simple calculator program
+        System.out.println(2+3);// Add
+        System.out.println(10-2);// Sub
+        System.out.println(10*10);// Mul
+        System.out.println(10/7);// div
+    }
+}
