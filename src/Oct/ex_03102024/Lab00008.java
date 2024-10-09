0a7d8d2bee9cc7363b2ca3d30762b45bc44c78ab
ashut <ashutosh.mall2@gmail.com>
2024-10-09 22:13:58 +0530

    All file

---
+++ src/Oct/ex_03102024/Lab00008.java
@@ -0,0 +1,7 @@
+package Oct.ex_03102024;
+
+public class Lab00008 {
+    public static void main(String[] args) {
+        System.out.println("ashutosh");
+    }
+}
--- src/Oct/ex_03102024/Lab002.java
+++ src/Oct/ex_03102024/Lab002.java
@@ -9,5 +9,6 @@ public class Lab002 {
     //
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("as");
     }
 }
