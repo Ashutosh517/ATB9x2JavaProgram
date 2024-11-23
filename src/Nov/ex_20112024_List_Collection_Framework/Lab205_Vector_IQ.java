d2a34e8b2323eb7c9093579f6deadd701568e0dd
ashut <ashutosh.mall2@gmail.com>
2024-11-24 00:55:31 +0530

    20 nov1 2024

--- src/Nov/ex_20112024_List_Collection_Framework/Lab205_Vector_IQ.java
+++ src/Nov/ex_20112024_List_Collection_Framework/Lab205_Vector_IQ.java
@@ -1,7 +1,29 @@
 package Nov.ex_20112024_List_Collection_Framework;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collection.*;
+
 public class Lab205_Vector_IQ {
     public static void main(String[] args) {
+
+        List marks = new ArrayList();
+        marks.add(91);
+        marks.add(95);
+        marks.add(56);
+        marks.add(89);
+
+        System.out.println(marks);
+        Collections.sort(marks);
+        System.out.println(marks);
+
+        Collections.sort(marks,Collections.reverseOrder());
+        System.out.println(marks);
+
+
 
     }
 }
